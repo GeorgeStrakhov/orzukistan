50184b0a3fa4f2d520fa9119020755b5a9ab39cd
George Strakhov <george.strakhov@gmail.com>
2016-03-19 21:26:31 +0300

    sound

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ## Info
 * Our image is of fixed size 8192x8192 px
 * Background is white
+* Sound depends on zoom and lat-long position
 
 ## How to run
 
 1. Install python 2.7
 2. Install gdal (`brew install gdal`)
 3. Make tiles like this: `gdal2tiles.py -p raster -z 0-5 -w none src/orzukistan.png` (More info here: http://build-failed.blogspot.ru/2012/11/zoomable-image-with-leaflet.html)
-4. Commit
-5. Push 
+4. `bower install`
+5. `python -m SimpleHTTPServer 8000`
 
 ## Useful links
 
@@ -16,9 +17,14 @@
 * http://blog.mikecouturier.com/2011/07/create-zoomable-images-using-google.html#maps_reading
 * http://www.thunderforest.com/tutorials/tile-format/
 
-# TODO
+# DONE
+* scaffold
+* make tiles
+* use gmaps
+* add sounds test
 
-* get started with the actual picture
+# TODO
 * write a project description and welcome note (handwriting) and set default lat long and zoom to it
-* get a domain name and push
+* get a domain name and push to gh-pages
 * add a copyright and fork me on github thingy to the bottom of the page
+* get started with the actual picture
---
+++ js/custom.js
@@ -0,0 +1,109 @@
+//vars
+var omuz = {};
+omuz.ready = false;
+omuz.maxVolume = 1;
+
+omuz.sounds = {
+  'bass': {
+      fileName: 'bass.ogg',
+      isLoaded: false,
+      zoomLevel: 3,
+    },
+  'piano1': {
+      fileName: 'piano1.ogg',
+      isLoaded: false,
+      zoomLevel: 1,
+    },
+  'piano2': {
+      fileName: 'piano2.ogg',
+      isLoaded: false,
+      zoomLevel: 3,
+    },
+  'dudka': {
+      fileName: 'dudka.ogg',
+      isLoaded: false,
+      zoomLevel: 5,
+    },
+  'pad': {
+      fileName: 'pad.ogg',
+      isLoaded: false,
+      zoomLevel: 0,
+    },
+  'guitar1': {
+      fileName: 'guitar1.ogg',
+      isLoaded: false,
+      zoomLevel: 5,
+    },
+  'guitar2': {
+      fileName: 'guitar2.ogg',
+      isLoaded: false,
+      zoomLevel: 4,
+    },
+  'drums': {
+      fileName: 'drums.ogg',
+      isLoaded: false,
+      zoomLevel: 4,
+    }
+};
+
+//init
+$(document).ready(function(){
+  console.log('doc ready. crancking up!');
+  loadSounds();
+});
+
+//loaded listener
+var startIfAllSoundsReady = function() {
+  var ready = true;
+  _.forEach(omuz.sounds, function(sval, sname) {
+    if(!sval.isLoaded) {
+      ready = false;
+    };
+  }); //end forEach
+  if(ready) {
+    omuz.ready = true;
+    initPlayback();
+  }
+};
+
+//load sounds
+var loadSounds = function() {
+  _.forEach(omuz.sounds, function(sval, sname) {
+    omuz.sounds[sname].howl = new Howl({
+      urls: ['/muz/'+sval.fileName],
+      loop: true,
+      onload: function() {
+        omuz.sounds[sname].isLoaded = true;
+        console.log('Loaded: '+sname);
+        startIfAllSoundsReady();
+      }
+    }); //end Howl
+  }); //end forEach
+};
+
+//start playback
+var initPlayback = function() {
+  console.log('playback started!');
+  _.forEach(omuz.sounds, function(sval, sname) {
+    omuz.sounds[sname].howl.volume(0); //start everything muted
+    omuz.sounds[sname].howl.play();
+  });//end forEach
+};
+
+//mute all
+omuz.stopMusic = function() {
+  _.forEach(omuz.sounds, function(sval, sname) {
+    omuz.sounds[sname].howl.volume(0); //start everything muted
+  });//end forEach
+};
+
+//update music based on map frame
+omuz.updateMusic = function(w) {
+  _.forEach(omuz.sounds, function(sval, sname) {
+    if(sval.zoomLevel <= w.zoom) {
+      omuz.sounds[sname].howl.fade(omuz.sounds[sname].howl.volume(), omuz.maxVolume, 1000);
+    } else {
+      omuz.sounds[sname].howl.fade(omuz.sounds[sname].howl.volume(), 0.0, 1000);
+    }
+  }); //end forEach
+};
